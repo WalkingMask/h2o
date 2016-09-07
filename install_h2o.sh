2743a5380f8fa25c43b39e117dc6fbf4d7010ac7
walkingmask <walkingmask@gmail.com>
2016-09-08 02:02:56 +0900

    fix

--- install_h2o.sh
+++ install_h2o.sh
@@ -37,18 +37,18 @@ fi
 
 if [ -e "/usr/local/etc/h2o/h2o.conf" ]; then
   rm /usr/local/etc/h2o/h2o.conf
-  mv ./h2o_isucon5.conf /usr/local/etc/h2o/h2o.conf
+  cp ./h2o_isucon5.conf /usr/local/etc/h2o/h2o.conf
 else
-  mv ./h2o_isucon5.conf /usr/local/etc/h2o/h2o.conf
+  cp ./h2o_isucon5.conf /usr/local/etc/h2o/h2o.conf
 fi
 
 if [ -e "/etc/systemd/system//h2o.service" ]; then
   rm /etc/systemd/system/h2o.service
-  mv ./h2o.service /etc/systemd/system/h2o.service
+  cp ./h2o.service /etc/systemd/system/h2o.service
   chmod 644 /etc/systemd/system/h2o.service
   chown root /etc/systemd/system/h2o.service
 else
-  mv ./h2o.service /etc/systemd/system/h2o.service
+  cp ./h2o.service /etc/systemd/system/h2o.service
   chmod 644 /etc/systemd/system/h2o.service
   chown root /etc/systemd/system/h2o.service
 fi
